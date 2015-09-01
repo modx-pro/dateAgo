76b0e0fdbec3e5f9ab16089a593824cd192c09f3
gvozdb <gvozdb@users.noreply.github.com>
2015-09-01 19:46:35 +0500

    Create default.inc.php

---
+++ core/components/dateago/lexicon/kz/default.inc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Default Kazakh Lexicon Entries for dateAgo
+ *
+ * @package dateago
+ * @subpackage lexicon
+ */
+$_lang['da_now'] = 'Қазір ғана';
+$_lang['da_today'] = 'Бүгін';
+$_lang['da_yesterday'] = 'Кеше';
+$_lang['da_tomorrow'] = 'Ертең';
+$_lang['da_minutes'] = '["[[+minutes]] минутқа жетпей","[[+minutes]] минутқа жетпей","[[+minutes]] минутқа жетпей"]';
+$_lang['da_minutes_less'] = 'бір минутқа жетпей';
+$_lang['da_hours'] = '["[[+hours]] сағатқа жетпей","[[+hours]] сағатқа жетпей","[[+hours]] сағатқа жетпей"]';
+$_lang['da_hours_less'] = 'бір сағатқа жетпей';
+$_lang['da_months'] = '["қаңтар","ақпан","наурыз","сәуір","мамыр","маусым","шілде","тамыз","қыркүйек","қазан","қараша","желтоксан"]';
