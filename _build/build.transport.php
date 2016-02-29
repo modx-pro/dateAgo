3be859cbf556d5640a33fdb4d481d23e8f4e5148
Vasily Naumkin <bezumkin@yandex.ru>
2016-02-29 19:58:15 +0700

    Update builder

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -2,7 +2,7 @@
 /**
  * dateAgo build script
  *
- * @package dateago 
+ * @package dateago
  * @subpackage build
  */
 $mtime = microtime();
@@ -14,15 +14,15 @@ set_time_limit(0);
 require_once 'build.config.php';
 
 /* define sources */
-$root = dirname(dirname(__FILE__)).'/';
+$root = dirname(dirname(__FILE__)) . '/';
 $sources = array(
-	'root' => $root,
-	'build' => $root . '_build/',
-	'data' => $root . '_build/data/',
-	'snippets' => $root.'core/components/'.PKG_NAME_LOWER.'/elements/snippets/',
-	'lexicon' => $root . 'core/components/'.PKG_NAME_LOWER.'/lexicon/',
-	'docs' => $root.'core/components/'.PKG_NAME_LOWER.'/docs/',
-	'source_core' => $root.'core/components/'.PKG_NAME_LOWER,
+    'root' => $root,
+    'build' => $root . '_build/',
+    'data' => $root . '_build/data/',
+    'snippets' => $root . 'core/components/' . PKG_NAME_LOWER . '/elements/snippets/',
+    'lexicon' => $root . 'core/components/' . PKG_NAME_LOWER . '/lexicon/',
+    'docs' => $root . 'core/components/' . PKG_NAME_LOWER . '/docs/',
+    'source_core' => $root . 'core/components/' . PKG_NAME_LOWER,
 );
 unset($root);
 
@@ -30,102 +30,99 @@ unset($root);
 require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
 require_once $sources['build'] . '/includes/functions.php';
 
-$modx= new modX();
+$modx = new modX();
 $modx->initialize('mgr');
 echo '<pre>'; /* used for nice formatting of log messages */
 $modx->setLogLevel(modX::LOG_LEVEL_INFO);
-$modx->getService('error','error.modError');
+$modx->getService('error', 'error.modError');
 $modx->setLogTarget('ECHO');
 
-$modx->loadClass('transport.modPackageBuilder','',false, true);
+$modx->loadClass('transport.modPackageBuilder', '', false, true);
 $builder = new modPackageBuilder($modx);
-$builder->createPackage(PKG_NAME_LOWER,PKG_VERSION,PKG_RELEASE);
-$builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
-$modx->log(modX::LOG_LEVEL_INFO,'Created Transport Package and Namespace.');
+$builder->createPackage(PKG_NAME_LOWER, PKG_VERSION, PKG_RELEASE);
+$builder->registerNamespace(PKG_NAME_LOWER, false, true, '{core_path}components/' . PKG_NAME_LOWER . '/');
+$modx->log(modX::LOG_LEVEL_INFO, 'Created Transport Package and Namespace.');
 
 /* add snippets */
-$snippets = include $sources['data'].'transport.snippets.php';
+$snippets = include $sources['data'] . 'transport.snippets.php';
 
 /* package in snippet */
-$attributes= array(
-	xPDOTransport::UNIQUE_KEY => 'name',
-	xPDOTransport::PRESERVE_KEYS => false,
-	xPDOTransport::UPDATE_OBJECT => true,
+$attributes = array(
+    xPDOTransport::UNIQUE_KEY => 'name',
+    xPDOTransport::PRESERVE_KEYS => false,
+    xPDOTransport::UPDATE_OBJECT => true,
 );
 $vehicle = $builder->createVehicle($snippets[0], $attributes);
-$vehicle->resolve('file',array(
-	'source' => $sources['source_core'],
-	'target' => "return MODX_CORE_PATH . 'components/';",
+$vehicle->resolve('file', array(
+    'source' => $sources['source_core'],
+    'target' => "return MODX_CORE_PATH . 'components/';",
 ));
-$modx->log(modX::LOG_LEVEL_INFO,'Adding file resolvers to category...');
-$vehicle->resolve('file',array(
-	'source' => $sources['source_core'],
-	'target' => "return MODX_CORE_PATH . 'components/';",
+$modx->log(modX::LOG_LEVEL_INFO, 'Adding file resolvers to category...');
+$vehicle->resolve('file', array(
+    'source' => $sources['source_core'],
+    'target' => "return MODX_CORE_PATH . 'components/';",
 ));
 $builder->putVehicle($vehicle);
-unset($properties,$snippet,$vehicle);
+unset($properties, $snippet, $vehicle);
 
 /* now pack in the license file, readme and setup options */
 $builder->setPackageAttributes(array(
-	'changelog' => file_get_contents($sources['docs'] . 'changelog.txt')
-	,'license' => file_get_contents($sources['docs'] . 'license.txt')
-	,'readme' => file_get_contents($sources['docs'] . 'readme.txt')
-	//'setup-options' => array(
-		//'source' => $sources['build'].'setup.options.php',
-	//),
+    'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
+    'license' => file_get_contents($sources['docs'] . 'license.txt'),
+    'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
 ));
-$modx->log(modX::LOG_LEVEL_INFO,'Added package attributes and setup options.');
+$modx->log(modX::LOG_LEVEL_INFO, 'Added package attributes and setup options.');
 
 /* zip up package */
-$modx->log(modX::LOG_LEVEL_INFO,'Packing up transport package zip...');
+$modx->log(modX::LOG_LEVEL_INFO, 'Packing up transport package zip...');
 $builder->pack();
 
-$mtime= microtime();
-$mtime= explode(" ", $mtime);
-$mtime= $mtime[1] + $mtime[0];
-$tend= $mtime;
-$totalTime= ($tend - $tstart);
-$totalTime= sprintf("%2.4f s", $totalTime);
+$mtime = microtime();
+$mtime = explode(" ", $mtime);
+$mtime = $mtime[1] + $mtime[0];
+$tend = $mtime;
+$totalTime = ($tend - $tstart);
+$totalTime = sprintf("%2.4f s", $totalTime);
 
 $signature = $builder->getSignature();
 if (defined('PKG_AUTO_INSTALL') && PKG_AUTO_INSTALL) {
-	$sig = explode('-',$signature);
-	$versionSignature = explode('.',$sig[1]);
+    $sig = explode('-', $signature);
+    $versionSignature = explode('.', $sig[1]);
 
-	/* @var modTransportPackage $package */
-	if (!$package = $modx->getObject('transport.modTransportPackage', array('signature' => $signature))) {
-		$package = $modx->newObject('transport.modTransportPackage');
-		$package->set('signature', $signature);
-		$package->fromArray(array(
-			'created' => date('Y-m-d h:i:s'),
-			'updated' => null,
-			'state' => 1,
-			'workspace' => 1,
-			'provider' => 0,
-			'source' => $signature.'.transport.zip',
-			'package_name' => $sig[0],
-			'version_major' => $versionSignature[0],
-			'version_minor' => !empty($versionSignature[1]) ? $versionSignature[1] : 0,
-			'version_patch' => !empty($versionSignature[2]) ? $versionSignature[2] : 0,
-		));
-		if (!empty($sig[2])) {
-			$r = preg_split('/([0-9]+)/',$sig[2],-1,PREG_SPLIT_DELIM_CAPTURE);
-			if (is_array($r) && !empty($r)) {
-				$package->set('release',$r[0]);
-				$package->set('release_index',(isset($r[1]) ? $r[1] : '0'));
-			} else {
-				$package->set('release',$sig[2]);
-			}
-		}
-		$package->save();
-	}
+    /* @var modTransportPackage $package */
+    if (!$package = $modx->getObject('transport.modTransportPackage', array('signature' => $signature))) {
+        $package = $modx->newObject('transport.modTransportPackage');
+        $package->set('signature', $signature);
+        $package->fromArray(array(
+            'created' => date('Y-m-d h:i:s'),
+            'updated' => null,
+            'state' => 1,
+            'workspace' => 1,
+            'provider' => 0,
+            'source' => $signature . '.transport.zip',
+            'package_name' => $sig[0],
+            'version_major' => $versionSignature[0],
+            'version_minor' => !empty($versionSignature[1]) ? $versionSignature[1] : 0,
+            'version_patch' => !empty($versionSignature[2]) ? $versionSignature[2] : 0,
+        ));
+        if (!empty($sig[2])) {
+            $r = preg_split('/([0-9]+)/', $sig[2], -1, PREG_SPLIT_DELIM_CAPTURE);
+            if (is_array($r) && !empty($r)) {
+                $package->set('release', $r[0]);
+                $package->set('release_index', (isset($r[1]) ? $r[1] : '0'));
+            } else {
+                $package->set('release', $sig[2]);
+            }
+        }
+        $package->save();
+    }
 
-	if ($package->install()) {
-		$modx->runProcessor('system/clearcache');
-	}
+    if ($package->install()) {
+        $modx->runProcessor('system/clearcache');
+    }
 }
 if (!empty($_GET['download'])) {
-	echo '<script>document.location.href = "/core/packages/' . $signature.'.transport.zip' . '";</script>';
+    echo '<script>document.location.href = "/core/packages/' . $signature . '.transport.zip' . '";</script>';
 }
 
-$modx->log(modX::LOG_LEVEL_INFO,"\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
+$modx->log(modX::LOG_LEVEL_INFO, "\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
